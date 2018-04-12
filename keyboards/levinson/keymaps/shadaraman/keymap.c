d5d002ab62938101c93c4c5b711eaf90c2fd8ed0
Shadaraman <37378261+Shadaraman@users.noreply.github.com>
2018-04-12 13:21:29 -0700

    Swap Nav and Adjust

--- keyboards/levinson/keymaps/shadaraman/keymap.c
+++ keyboards/levinson/keymaps/shadaraman/keymap.c
@@ -57,14 +57,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |Shift |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |Adjust| GUI  | Nav  |Lower |Enter |Space |Raise | Alt  |      |      |      |
+ * | Ctrl | Nav  | GUI  |Adjust|Lower |Enter |Space |Raise | Alt  |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = KEYMAP( \
   KC_GESC,  KC_Q,    KC_W,    KC_E,  KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_TAB,   KC_A,    KC_S,    KC_D,  KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_OSFT,  KC_Z,    KC_X,    KC_C,  KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_OSFT, \
-  KC_LCTL,  ADJUST,  KC_LGUI, NAV,   SYMBOL,  KC_ENT,  KC_SPC,  NUMBER,  KC_LALT, _______, _______, _______ \
+  KC_LCTL,  NAV,     KC_LGUI, ADJUST,SYMBOL,  KC_ENT,  KC_SPC,  NUMBER,  KC_LALT, _______, _______, _______ \
 ),
 
 /* Colemak (Mod-DH)
@@ -75,14 +75,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |Shift |   Z  |   X  |   C  |   D  |   V  |   M  |   H  |   ,  |   .  |   /  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl |Adjust| GUI  | Nav  |Lower |Enter |Space |Raise | Alt  |      |      |      |
+ * | Ctrl | Nav  | GUI  |Adjust|Lower |Enter |Space |Raise | Alt  |      |      |      |
  * `-----------------------------------------------------------------------------------'
  */
 [_COLEMAK] = KEYMAP( \
   KC_GESC,  KC_Q,    KC_W,    KC_F,  KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC, \
   KC_TAB,   KC_A,    KC_R,    KC_S,  KC_T,    KC_G,    KC_K,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
   KC_OSFT,  KC_Z,    KC_X,    KC_C,  KC_D,    KC_V,    KC_M,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_OSFT, \
-  KC_LCTL,  ADJUST,  KC_LGUI, NAV,   SYMBOL,  KC_ENT,  KC_SPC,  NUMBER,  KC_LALT, _______, _______, _______ \
+  KC_LCTL,  NAV,     KC_LGUI, ADJUST,SYMBOL,  KC_ENT,  KC_SPC,  NUMBER,  KC_LALT, _______, _______, _______ \
 ),
 
 /* Lower
