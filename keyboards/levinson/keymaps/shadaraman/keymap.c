9b1718508787cbd1c724dad95c54a83252e7e60f
Shadaraman <37378261+Shadaraman@users.noreply.github.com>
2018-04-12 14:12:10 -0700

    Map Nav to Sym + Num. Vol controls on base layer.

--- keyboards/levinson/keymaps/shadaraman/keymap.c
+++ keyboards/levinson/keymaps/shadaraman/keymap.c
@@ -57,14 +57,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |Shift |   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |   /  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Nav  | GUI  |Adjust|Lower |Enter |Space |Raise | Alt  |      |      |      |
+ * | Ctrl | Nav  | GUI  |Adjust|Lower |Enter |Space |Raise | Alt  | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_QWERTY] = KEYMAP( \
   KC_GESC,  KC_Q,    KC_W,    KC_E,  KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_TAB,   KC_A,    KC_S,    KC_D,  KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_OSFT,  KC_Z,    KC_X,    KC_C,  KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_OSFT, \
-  KC_LCTL,  NAV,     KC_LGUI, ADJUST,SYMBOL,  KC_ENT,  KC_SPC,  NUMBER,  KC_LALT, _______, _______, _______ \
+  KC_LCTL,  NAV,     KC_LGUI, ADJUST,SYMBOL,  KC_ENT,  KC_SPC,  NUMBER,  KC_LALT, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
 
 /* Colemak (Mod-DH)
@@ -75,14 +75,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |Shift |   Z  |   X  |   C  |   D  |   V  |   M  |   H  |   ,  |   .  |   /  |Shift |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * | Ctrl | Nav  | GUI  |Adjust|Lower |Enter |Space |Raise | Alt  |      |      |      |
+ * | Ctrl | Nav  | GUI  |Adjust|Lower |Enter |Space |Raise | Alt  | Vol- | Vol+ | Play |
  * `-----------------------------------------------------------------------------------'
  */
 [_COLEMAK] = KEYMAP( \
   KC_GESC,  KC_Q,    KC_W,    KC_F,  KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC, \
   KC_TAB,   KC_A,    KC_R,    KC_S,  KC_T,    KC_G,    KC_K,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
   KC_OSFT,  KC_Z,    KC_X,    KC_C,  KC_D,    KC_V,    KC_M,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_OSFT, \
-  KC_LCTL,  NAV,     KC_LGUI, ADJUST,SYMBOL,  KC_ENT,  KC_SPC,  NUMBER,  KC_LALT, _______, _______, _______ \
+  KC_LCTL,  NAV,     KC_LGUI, ADJUST,SYMBOL,  KC_ENT,  KC_SPC,  NUMBER,  KC_LALT, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
 
 /* Lower
@@ -125,7 +125,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,-----------------------------------------------------------------------------------.
  * |   `  |   1  |   2  |   3  |   4  |   5  |   6  |   7  |   8  |   9  |   0  | Del  |
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * |      |      |      |      |      |      |      |   +  |   =  |   _  |   -  |      |
+ * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
  * |      |      |      |      |      |      |      |      |      |      |      |      |
  * |------+------+------+------+------+------+------+------+------+------+------+------|
@@ -134,7 +134,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_NUMBER] = KEYMAP( \
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL, \
-  _______, _______, _______, _______, _______, _______, _______, KC_PLUS, KC_EQL,  KC_UNDS, KC_MINS, _______, \
+  _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
   _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
 ),
@@ -250,20 +250,20 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case NUMBER:
       if (record->event.pressed) {
         layer_on(_NUMBER);
-        update_tri_layer(_NUMBER, _SYMBOL, _ADJUST);
+        update_tri_layer(_NUMBER, _SYMBOL, _NAV);
       } else {
         layer_off(_NUMBER);
-        update_tri_layer(_NUMBER, _SYMBOL, _ADJUST);
+        update_tri_layer(_NUMBER, _SYMBOL, _NAV);
       }
       return false;
       break;
     case SYMBOL:
       if (record->event.pressed) {
         layer_on(_SYMBOL);
-        update_tri_layer(_NUMBER, _SYMBOL, _ADJUST);
+        update_tri_layer(_NUMBER, _SYMBOL, _NAV);
       } else {
         layer_off(_SYMBOL);
-        update_tri_layer(_NUMBER, _SYMBOL, _ADJUST);
+        update_tri_layer(_NUMBER, _SYMBOL, _NAV);
       }
       return false;
       break;
