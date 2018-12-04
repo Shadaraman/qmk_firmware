cd11421ad85170419b1fb98794da6a167370e223
Shadaraman <37378261+Shadaraman@users.noreply.github.com>
2018-12-04 07:54:31 -0800

    Swap space and enter back.

--- keyboards/levinson/keymaps/shadaraman/keymap.c
+++ keyboards/levinson/keymaps/shadaraman/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_E,  KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_CBSP, KC_A,    KC_S,    KC_D,  KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, \
   KC_LSPO, KC_Z,    KC_X,    KC_C,  KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, \
-  KC_ESC,  NAV,     KC_LALT, ADJUST,SYMBOL,  KC_SPC,  KC_ENT,  NUMBER,  KC_LGUI, KC_VOLD, KC_VOLU, KC_MPLY \
+  KC_ESC,  NAV,     KC_LALT, ADJUST,SYMBOL,  KC_ENT,  KC_SPC,  NUMBER,  KC_LGUI, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
 
 /* Colemak (Mod-DH)
@@ -83,7 +83,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,  KC_Q,    KC_W,    KC_F,  KC_P,    KC_B,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_BSPC, \
   KC_CBSP, KC_A,    KC_R,    KC_S,  KC_T,    KC_G,    KC_K,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT, \
   KC_LSPO, KC_Z,    KC_X,    KC_C,  KC_D,    KC_V,    KC_M,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, \
-  KC_ESC,  NAV,     KC_LALT, ADJUST,SYMBOL,  KC_SPC,  KC_ENT,  NUMBER,  KC_LGUI, KC_VOLD, KC_VOLU, KC_MPLY \
+  KC_ESC,  NAV,     KC_LALT, ADJUST,SYMBOL,  KC_ENT,  KC_SPC,  NUMBER,  KC_LGUI, KC_VOLD, KC_VOLU, KC_MPLY \
 ),
 
 /* Lower
